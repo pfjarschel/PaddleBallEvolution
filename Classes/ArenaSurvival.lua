d4617ce5410ba5d34032093f7bbed1c6d4a47b7c
Paulo Felipe Jarschel <pfjarschel@gmail.com>
2013-02-25 21:12:20 -0300

    Added Scene Manager
    And major code clean-up

--- Classes/ArenaSurvival.lua
+++ Classes/ArenaSurvival.lua
@@ -4,7 +4,7 @@
 
 ArenaSurvival = Core.class(Sprite)
 
--- Declare boundaries object
+-- Declare Stuff --
 ArenaSurvival.bitmap = nil
 ArenaSurvival.bounds = nil
 ArenaSurvival.score0 = 0
@@ -13,6 +13,7 @@ ArenaSurvival.difFactor = 5/5 -- 1/5 to 10/5
 ArenaSurvival.paused = false
 ArenaSurvival.font = nil
 
+-- Main Match Loop --
 local function onEnterFrame()
 	updatePhysics()
 	arena:moveAI()
@@ -20,19 +21,32 @@ local function onEnterFrame()
 	arena:checkGoal()
 end
 
--- Initialization function
-function ArenaSurvival:init(difficulty)
-	self.font = fonts.arialroundedMed
-	self.bitmap = textures.pongbg
-	self.bitmap:setScale(1, 1)
-	self.difFactor = difficulty/5
-	self:addChild(self.bitmap)
-	local textureW = self.bitmap:getWidth()
-	local textureH = self.bitmap:getHeight()
-	self.bitmap:setScale(WX/textureW, WY/textureH)
-	self:setAlpha(0)
-	self:createBoundaries()
+-- Create physics stuff (including collision handler) --
+function ArenaSurvival:createBoundaries()
 	
+	-- Default is Static --
+	self.bounds = world:createBody({})
+	
+	self.bounds.name = "bounds"
+	local shapeT = b2.EdgeShape.new(-200, WBounds, WX + 200, WBounds)
+	local shapeB = b2.EdgeShape.new(-200, WY-WBounds, WX + 200, WY-WBounds)
+	self.bounds:createFixture{
+		shape = shapeT, 
+		friction = 0,
+	}
+	self.bounds:createFixture{
+		shape = shapeB, 
+		friction = 0,
+	}
+	
+	-- Collision Handler --
+	function self.bounds:collide(event)
+		--print("bounds")
+	end
+end
+
+-- This function creates the in-game menu --
+function ArenaSurvival:addMenu()
 	local menuBut = MenuBut.new(textures.menuBut, 40, 40)
 	menuBut.bitmap:setPosition(WX/2, WY - menuBut.bitmap:getHeight()/2)
 	menuBut:setAlpha(0.4)
@@ -40,7 +54,7 @@ function ArenaSurvival:init(difficulty)
 	menuBut:addEventListener(Event.TOUCHES_BEGIN, function(event)
 		if menuBut:hitTestPoint(event.touch.x, event.touch.y) then
 			event:stopPropagation()
-			if self.paused then else
+			if not self.paused then
 				self.paused = true
 				self:removeEventListener(Event.ENTER_FRAME, onEnterFrame)
 				
@@ -58,20 +72,21 @@ function ArenaSurvival:init(difficulty)
 					end
 				end)
 				pausebg:addChild(resumeBut)
+				
 				local quitBut = MenuBut.new(textures.exitBut, 150, 40)
 				quitBut.bitmap:setPosition(WX/2, WY/2 + quitBut:getHeight())
 				quitBut:addEventListener(Event.TOUCHES_BEGIN, function(event)
 					if quitBut:hitTestPoint(event.touch.x, event.touch.y) then
 						event:stopPropagation()
 						stage:removeChild(pausebg)
-						stage:removeChild(self)
 						world:destroyBody(self.ball.body)
 						self.ball.body = nil
 						world:destroyBody(self.leftPlayer.paddle.body)
 						world:destroyBody(self.rightPlayer.paddle.body)
 						world:destroyBody(self.bounds)
 						self = nil
-						loadMainMenu()
+						arena = nil
+						sceneMan:changeScene("mainMenu", transTime, SceneManager.fade, easing.linear)
 					end
 				end)
 				pausebg:addChild(quitBut)
@@ -80,55 +95,74 @@ function ArenaSurvival:init(difficulty)
 			end
 		end
 	end)
+end
+
+-- Function to Handle the End of the Match -- 
+function ArenaSurvival:gameOver()
+	self:removeEventListener(Event.ENTER_FRAME, onEnterFrame)
+	fadeOut(self)
 	
-	stage:addChild(self)
-	fadeIn(self)
-	Timer.delayedCall(0, function()
-		self:createArenaChildren()
+	local gameOverString = nil
+	gameOverString = "You managed to survive " .. tostring(self.score0) .. " rounds!"
+	sounds.lose:play()
+	local gameOverTextBox = TextField.new(self.font, gameOverString)
+	gameOverTextBox:setTextColor(0x3c78a0)
+	gameOverTextBox:setPosition(0.5*WX - gameOverTextBox:getWidth()/2, 0.25*WY + gameOverTextBox:getHeight()/2)
+	
+	local againBut = MenuBut.new(textures.againBut, 150, 40)
+	againBut.bitmap:setPosition(WX/2, WY/2 + 100)
+	local returnBut = MenuBut.new(textures.returnBut, 150, 40)
+	returnBut.bitmap:setPosition(returnBut:getWidth()/2 + 10, WY/2 + 210)
+	
+	againBut:addEventListener(Event.TOUCHES_BEGIN, function(event)
+		if againBut:hitTestPoint(event.touch.x, event.touch.y) then
+			stage:removeChild(gameOverTextBox)
+			stage:removeChild(returnBut)
+			stage:removeChild(againBut)
+			world:destroyBody(self.ball.body)
+			self.ball.body = nil
+			world:destroyBody(self.leftPlayer.paddle.body)
+			world:destroyBody(self.rightPlayer.paddle.body)
+			world:destroyBody(self.bounds)
+			self = nil
+			arena = nil
+			sceneMan:changeScene("survival", transTime, SceneManager.fade, easing.linear, { userData = "5" })
+		end
+	end)
+	
+	returnBut:addEventListener(Event.TOUCHES_BEGIN, function(event)
+		if returnBut:hitTestPoint(event.touch.x, event.touch.y) then
+			stage:removeChild(gameOverTextBox)
+			stage:removeChild(returnBut)
+			stage:removeChild(againBut)
+			world:destroyBody(self.ball.body)
+			self.ball.body = nil
+			world:destroyBody(self.leftPlayer.paddle.body)
+			world:destroyBody(self.rightPlayer.paddle.body)
+			world:destroyBody(self.bounds)
+			self = nil
+			arena = nil
+			sceneMan:changeScene("mainMenu", transTime, SceneManager.fade, easing.linear)
+		end
+	end)
+	
+	Timer.delayedCall(transTime/2, function ()
+		stage:addChild(gameOverTextBox)	
+		stage:addChild(returnBut)
+		stage:addChild(againBut)
 	end)
 end
 
--- Create physics stuff (including collision handler)
-function ArenaSurvival:createBoundaries()
-	self.bounds = world:createBody({}) --Default is Static
-	self.bounds.name = "bounds"
-	local shapeT = b2.EdgeShape.new(-200, WBounds, WX + 200, WBounds)
-	local shapeB = b2.EdgeShape.new(-200, WY-WBounds, WX + 200, WY-WBounds)
-	self.bounds:createFixture{
-		shape = shapeT, 
-		friction = 0,
-		filter = {categoryBits = 0x0001, maskBits = 0xFFFF} -- Collides with everything
-	}
-	self.bounds:createFixture{
-		shape = shapeB, 
-		friction = 0,
-		filter = {categoryBits = 0x0001, maskBits = 0xFFFF}
-	}
-	function self.bounds:collide(event)
-		--print("bounds")
-	end
-end
-
--- This function creates the arena stuff
-function ArenaSurvival:createArenaChildren()
-	self.ball = Ball.new(self.difFactor)
-	self.leftPlayer = Player.new(0, false, self.difFactor, "survivalLeft")
-	self.rightPlayer = Player.new(1, false, self.difFactor, "survivalRight")
-	self.ball.baseSpeed = self.ball.baseSpeed*self.leftPlayer.char.atkFactor
-	self.leftPlayer.paddle.body.atkFactor = 1
-	self.rightPlayer.paddle.body.atkFactor = 1
-	self.combatStats = CombatStats.new()
-	self.combatStats:update(self.score0,self.score1)
-	self.ball:reset()
-	self:addEventListener(Event.ENTER_FRAME, onEnterFrame)
-end
-
--- Function to handle goals
+-- Function to handle goals --
 function ArenaSurvival:checkGoal()
 	local ballX, ballY = self.ball.body:getPosition()
-	local function updateLeft()
+	
+	-- Updates match attributes, each 2 goals, and the other normal stuff --
+	local function updateAttrs()
 		self.ball.baseSpeed = self.ball.baseSpeed/self.leftPlayer.char.atkFactor
+		
 		self.combatStats:update(self.score0, self.score1)
+		
 		if self.score0%2 == 0 then
 			if self.leftPlayer.char.atk < 50 then
 				self.leftPlayer.char.atk = self.leftPlayer.char.atk + 1
@@ -138,21 +172,25 @@ function ArenaSurvival:checkGoal()
 		else
 			sounds.goal1:play()
 		end
+		
 		if self.score0%4 == 0 then
 			if self.leftPlayer.char.def > 1 then
 				self.leftPlayer.char.def = self.leftPlayer.char.def - 1
 			end
 		end
+		
 		if self.score0%6 == 0 then
 			if self.rightPlayer.char.def < 30 then
 				self.rightPlayer.char.def = self.rightPlayer.char.def + 1
 			end
 		end
+		
 		if self.score0%8 == 0 then
 			if self.rightPlayer.char.int < 30 then
 				self.rightPlayer.char.int = self.rightPlayer.char.int + 1
 			end
 		end
+		
 		self.leftPlayer.char:updateAttr()
 		self.rightPlayer.char:updateAttr()
 		self.ball.baseSpeed = self.ball.baseSpeed*self.leftPlayer.char.atkFactor
@@ -168,78 +206,63 @@ function ArenaSurvival:checkGoal()
 		world:destroyBody(self.rightPlayer.paddle.body)
 		self.rightPlayer.paddle:createBody()
 		self.rightPlayer.paddle.body.atkFactor = 1
+		
 		self.ball:reset()
+		self.ball:launch()
 		self.leftPlayer.paddle:reset()
 		self.rightPlayer.paddle:reset()
 		gc()
 	end
-	local function gameOver()
-		self:removeEventListener(Event.ENTER_FRAME, onEnterFrame)
-		fadeOut(self)
-		local gameOverString = nil
-		gameOverString = "You managed to survive " .. tostring(self.score0) .. " rounds!"
-		sounds.lose:play()
-		local gameOverTextBox = TextField.new(self.font, gameOverString)
-		gameOverTextBox:setTextColor(0x3c78a0)
-		gameOverTextBox:setPosition(0.5*WX - gameOverTextBox:getWidth()/2, 0.25*WY + gameOverTextBox:getHeight()/2)
-		
-		local againBut = MenuBut.new(textures.againBut, 150, 40)
-		againBut.bitmap:setPosition(WX/2, WY/2 + 100)
-		local returnBut = MenuBut.new(textures.returnBut, 150, 40)
-		returnBut.bitmap:setPosition(returnBut:getWidth()/2 + 10, WY/2 + 210)
-		
-		againBut:addEventListener(Event.TOUCHES_BEGIN, function(event)
-			if againBut:hitTestPoint(event.touch.x, event.touch.y) then
-				stage:removeChild(gameOverTextBox)
-				stage:removeChild(returnBut)
-				stage:removeChild(againBut)
-				stage:removeChild(self)
-				world:destroyBody(self.ball.body)
-				self.ball.body = nil
-				world:destroyBody(self.leftPlayer.paddle.body)
-				world:destroyBody(self.rightPlayer.paddle.body)
-				world:destroyBody(self.bounds)
-				self = nil
-				loadArena("survival", 5)
-			end
-		end)
-		returnBut:addEventListener(Event.TOUCHES_BEGIN, function(event)
-			if returnBut:hitTestPoint(event.touch.x, event.touch.y) then
-				stage:removeChild(gameOverTextBox)
-				stage:removeChild(returnBut)
-				stage:removeChild(againBut)
-				stage:removeChild(self)
-				world:destroyBody(self.ball.body)
-				self.ball.body = nil
-				world:destroyBody(self.leftPlayer.paddle.body)
-				world:destroyBody(self.rightPlayer.paddle.body)
-				world:destroyBody(self.bounds)
-				self = nil
-				loadMainMenu()
-			end
-		end)
-		
-		Timer.delayedCall(700, function ()
-			stage:addChild(gameOverTextBox)	
-			stage:addChild(returnBut)
-			stage:addChild(againBut)
-		end)
-	end
+	
 	if ballX > WX + 2*self.ball.radius then
 		self.score0 = self.score0 + 1
-		updateLeft()
+		updateAttrs()
 	elseif ballX <  -2*self.ball.radius then
 		self.score1 = self.score1 + 1
-		gameOver()
+		self:gameOver()
 	end
 end
 
+-- Calls AI movement routines --
 function ArenaSurvival:moveAI()
 	self.leftPlayer:aiMove(self.ball)
 	self.rightPlayer:aiMove(self.ball)
 end
 
+-- Handles Input --
 function ArenaSurvival:moveHuman()
 	self.leftPlayer:humanMove()
 	self.rightPlayer:humanMove()
+end
+
+-- Initialization --
+function ArenaSurvival:init(difficulty)
+	arena = self
+	self.font = fonts.arialroundedMed
+	self.bitmap = textures.pongbg
+	self.bitmap:setScale(1, 1)
+	self.difFactor = difficulty/5
+	self:addChild(self.bitmap)
+	local textureW = self.bitmap:getWidth()
+	local textureH = self.bitmap:getHeight()
+	self.bitmap:setScale(WX/textureW, WY/textureH)
+	
+	self:addMenu()
+	
+	self:createBoundaries()
+	
+	self.ball = Ball.new(self.difFactor)
+	self.leftPlayer = Player.new(0, true, self.difFactor, "survivalLeft")
+	self.rightPlayer = Player.new(1, false, self.difFactor, "survivalRight")
+	self.ball.baseSpeed = self.ball.baseSpeed*self.leftPlayer.char.atkFactor
+	self.leftPlayer.paddle.body.atkFactor = 1
+	self.rightPlayer.paddle.body.atkFactor = 1
+	self.combatStats = CombatStats.new()
+	self.combatStats:update(self.score0, self.score1)
+	self.ball:reset()
+	
+	self:addEventListener("enterEnd", function()
+		self.ball:launch()
+	end)
+	self:addEventListener(Event.ENTER_FRAME, onEnterFrame)
 end
---
+++ Classes/MainMenu_Classic.lua
@@ -0,0 +1,78 @@
+--------------------------------------------
+-- Class for the Main Menu - Classic Mode --
+--------------------------------------------
+
+MainMenu_Classic = Core.class(Sprite)
+
+-- Declarations --
+MainMenu_Classic.font = nil
+
+-- Initialization --
+function MainMenu_Classic:init()
+	self.font = fonts.arialroundedSmall
+	
+	-- If the same bg as the main menu one is used, weird stuff happens --
+	local menubg = textures.mainmenubg2
+	menubg:setScale(1, 1)
+	local textureW = menubg:getWidth()
+	local textureH = menubg:getHeight()
+	menubg:setScale(WX/textureW, WY/textureH)
+	self:addChild(menubg)
+
+	-- Difficulty Selection --
+	local difTextBox = TextField.new(self.font, "Difficulty:")
+	difTextBox:setTextColor(0x000000)
+	difTextBox:setPosition(0.5*WX - difTextBox:getWidth()/2, 0.5*WY + 60)
+	self:addChild(difTextBox)
+	local difficulty = 5
+	local numTextBox = TextField.new(self.font, tostring(difficulty))
+	numTextBox:setTextColor(0x000000)
+	numTextBox:setPosition(0.5*WX - numTextBox:getWidth()/2, 0.5*WY + 115)
+	self:addChild(numTextBox)
+	
+	self.decDif = MenuBut.new(textures.minusBut, 40, 40)
+	self:addChild(self.decDif)
+	self.decDif.bitmap:setPosition(WX/2 - 60, WY/2 + 100)
+	self.decDif:addEventListener(Event.TOUCHES_BEGIN, function(event)
+		if self.decDif:hitTestPoint(event.touch.x, event.touch.y) then
+			difficulty = difficulty - 1
+			if difficulty < 1 then difficulty = 1 end
+			self:removeChild(numTextBox)
+			numTextBox = TextField.new(self.font, tostring(difficulty))
+			numTextBox:setPosition(0.5*WX - numTextBox:getWidth()/2, 0.5*WY + 115)
+			self:addChild(numTextBox)
+		end
+	end)
+	
+	self.incDif = MenuBut.new(textures.plusBut, 40, 40)
+	self:addChild(self.incDif)
+	self.incDif.bitmap:setPosition(WX/2 + 60, WY/2 + 100)
+	self.incDif:addEventListener(Event.TOUCHES_BEGIN, function(event)
+		if self.incDif:hitTestPoint(event.touch.x, event.touch.y) then
+			difficulty = difficulty + 1
+			if difficulty > 10 then difficulty = 10 end
+			self:removeChild(numTextBox)
+			numTextBox = TextField.new(self.font, tostring(difficulty))
+			numTextBox:setPosition(0.5*WX - numTextBox:getWidth()/2, 0.5*WY + 115)
+			self:addChild(numTextBox)
+		end
+	end)
+	
+	self.goBut = MenuBut.new(textures.goBut, 150, 40)
+	self:addChild(self.goBut)
+	self.goBut.bitmap:setPosition(WX/2, WY/2 + 160)
+	self.goBut:addEventListener(Event.TOUCHES_BEGIN, function(event)
+		if self.goBut:hitTestPoint(event.touch.x, event.touch.y) then
+			sceneMan:changeScene("classic", transTime, SceneManager.fade, easing.linear, { userData = difficulty }) 
+		end
+	end)
+	
+	self.returnBut = MenuBut.new(textures.returnBut, 150, 40)
+	self:addChild(self.returnBut)
+	self.returnBut.bitmap:setPosition(self.returnBut:getWidth()/2 + 10, WY/2 + 210)
+	self.returnBut:addEventListener(Event.TOUCHES_BEGIN, function(event)
+		if self.returnBut:hitTestPoint(event.touch.x, event.touch.y) then
+			sceneMan:changeScene("mainMenu", transTime, SceneManager.fade, easing.linear) 
+		end
+	end)
+end
